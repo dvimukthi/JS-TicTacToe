0474437ae5e6291248058e4c560ac1973404e8e8
dvimukthi <deshanvimukthi59@gmail.com>
2022-09-02 16:24:29 +0530

    WinConditions

---
+++ src/main.js
@@ -0,0 +1,18 @@
+const cells = document.querySelectorAll(".Cell");
+const StatusText = document.querySelector("#StatusText");
+const RestartBtn = document.querySelector("#RestartBtn");
+
+// win conditions
+const WinConditions = [
+  // rows
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  // columns
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  // diagonals
+  [0, 4, 8],
+  [2, 4, 6],
+];
